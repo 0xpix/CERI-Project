825d087f03dd15d29b15befeca9dbbac279a626a
Annes Chelfat <ns.chlfat@gmail.com>
2024-05-28 22:58:28 +0200

    Update download_data.py

--- src/data/download_data.py
+++ src/data/download_data.py
@@ -11,8 +11,8 @@ files_to_download = {
         # Add more file IDs and paths as needed
     },
     "processed": {
-        "17GVeNmIU-TYyuN-MtDeci1V64KHul_KA": "data/external/Landscan_data/processed/Landscan_clipped_Africa.zip",
-        "1_jjy2QOnTL7Bl8wVukgcOZbHS2I-7wNZ": "data/external/Landscan_data/processed/Landscan_clipped_country_level.zip",
+        "17GVeNmIU-TYyuN-MtDeci1V64KHul_KA": "data/external/Landscan_data/processed/africa/Landscan_clipped_Africa.zip",
+        "1_jjy2QOnTL7Bl8wVukgcOZbHS2I-7wNZ": "data/external/Landscan_data/processed/country_level/Landscan_clipped_country_level.zip",
         # Add more file IDs and paths as needed
     }
 }
